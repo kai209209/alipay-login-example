a3befe64e968867015cc984751f1f709b1998f8c
zky <361561797@qq.com>
2015-09-06 17:00:53 +0800

    设置请求快捷登录的接口与返回接口

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.redirect_to_alipay_login_gateway
+    options = { "service" => "alipay.auth.authorize",
+                "partner" => "alipay_pid: 123456", 
+                "_input_charset" => "utf-8", 
+                "return_url" =>  "abc.com:3000(本地调试)", 
+                "target_service" => "user.auth.quick.login" }
+    options.merge!("sign_type" => "MD5", 
+                   "sign" => Digest::MD5.hexdigest(options.sort.map{|k, v| "#{k}=#{v}"}.join("&") + "alipay_key: 123456"))
+    "https://mapi.alipay.com/gateway.do?#{options.sort.map{|k,v|"#{CGI::escape(k.to_s)}=#{CGI::escape(v.to_s)}"}.join("&")}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resource :user, only: :show
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_scope :user do
+    get 'alipay_login', to: redirect(User.redirect_to_alipay_login_gateway)
+    get 'sign_from_alipay', to: 'users/sessions#sign_from_alipay'
+  end
   root 'static#index'
 end
